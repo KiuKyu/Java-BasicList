8c0eaa5f5cdfd501ff203225418ee436e6d41c15
Kiu-Kyu <minkiukiu@gmail.com>
2022-02-07 00:51:42 +0700

    2nd

--- src/MyList.java
+++ src/MyList.java
@@ -1,30 +1,35 @@
 import java.util.Arrays;
 
-public class MyList<E> {
+public class MyList<E> { // E là Generic
     private int size = 0;
-    private static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 10; // sức chứa
     private Object[] elements;
 
     public MyList() {
-        elements = new Object[DEFAULT_CAPACITY];
+        this.elements = new Object[DEFAULT_CAPACITY]; // mảng chứa 10 phần tử
     }
 
     private void ensureCapacity() {
-        int newSize = elements.length * 2;
-        elements = Arrays.copyOf(elements, newSize);
+//        int newCapacity = DEFAULT_CAPACITY * 2;
+//          => Không dùng DEFAULT_CAPACITY do chỉ đúng cho lần đầu khởi tạo mảng do đây là 1 hằng số
+//          nên về sau thì nó sẽ luôn chỉ là 10 * 2
+        int newCapacity = elements.length * 2; // khai báo 1 biến newCapacity = x2 sức chứa mảng cũ
+        this.elements = Arrays.copyOf(this.elements, newCapacity); // hàm Arrays.copyOf truyền vào 2 tham số
+//        tham số thứ 1 : mảng cũ (để copy phần tử), tham số thứ 2 = capacity mới
     }
 
     public void add(E e) {
-        if (size == elements.length) {
+        if (size == elements.length) { // nếu size (số lượng phần tử trong mảng = sức chứa thì ...)
             ensureCapacity();
         }
-        elements[size++] = e;
+        this.elements[this.size++] = e; // sau khi thêm e vào mảng thì this.size ++
     }
 
-    public E get(int i) {
-        if (i >= size || i < 0) {
-            throw new IndexOutOfBoundsException("Index: " + i + ", Size" + size);
+    public E get(int index) { // Lấy ra giá trị của 1 phần tử trong mảng theo vị trí tương ứng
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size" + size);
+                // Nếu vị trí k hợp lệ thì ném ra lỗi
         }
-        return (E) elements[i];
+        return (E) this.elements[index];
     }
 }
